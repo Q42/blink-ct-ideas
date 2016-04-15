e94508cf197f180dc38b3717d7efeadf3fdceada
Lukas van Driel <lukas@q42.nl>
2016-04-15 19:22:58 +0200

    --owner option in deploy added mail_url

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,1 @@
-DEPLOY_HOSTNAME=galaxy.meteor.com meteor deploy juniorcomputerlab.q42.com --settings settings-prod.json
-# DEPLOY_HOSTNAME=galaxy.meteor.com meteor authorized juniorcomputerlab.q42.com --transfer q42
+DEPLOY_HOSTNAME=galaxy.meteor.com meteor deploy juniorcomputerlab.q42.nl --settings settings-prod.json --owner q42
