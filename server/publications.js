4f74bb7e9b9d601e72c8ad93c57210a38ae8b261
Lukas van Driel <lukas@q42.nl>
2016-04-15 19:23:37 +0200

    privacy, don't give mailaddress

--- server/publications.js
+++ server/publications.js
@@ -1,6 +1,7 @@
 import { Ideas } from '/imports/collections';
 
 const ITEMS_PER_PAGE = 10;
+const OVERVIEW_FIELDS = {authors: 1, title: 1, description: 1, attachments: 1};
 
 Meteor.publish('ideas.paged', (page: number = 1):object => {
   page = Math.max(page, 1);
@@ -8,17 +9,19 @@ Meteor.publish('ideas.paged', (page: number = 1):object => {
   return Ideas.find({}, {
     sort: { updatedDate: -1 },
     limit: ITEMS_PER_PAGE,
-    skip: (page-1)*ITEMS_PER_PAGE
+    skip: (page-1)*ITEMS_PER_PAGE,
+    fields: OVERVIEW_FIELDS
   });
 });
 
 Meteor.publish('ideas.all', () => {
   console.log('publishing ideas.all');
   return Ideas.find({}, {
-    sort: { updatedDate: -1 }
+    sort: { updatedDate: -1 },
+    fields: OVERVIEW_FIELDS
   });
 });
 
 Meteor.publish('idea', (id: string):object => {
-  return Ideas.find({_id:id});
+  return Ideas.find({_id:id}, {fields: {email: -1}});
 });
