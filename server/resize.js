beaf356b05598d38843797832c9fb283c216af8c
Joel de Nes <joeldenes@gmail.com>
2016-05-19 14:08:16 +0200

    Updates resize to work on insert iso update

--- server/resize.js
+++ server/resize.js
@@ -1,27 +1,29 @@
 import { Ideas } from '/imports/collections';
 import { HTTP } from 'meteor/http';
 
-Ideas.after.update((userId, doc, fieldNames, modifier, options) => {
-  const attachment = modifier['$push'] && modifier['$push'].attachments;
-  if (!attachment) return;
+Ideas.after.insert((userId, doc) => {
+  const attachments = doc.attachments;
+  if(!attachments || attachments.length === 0) return;
 
-  if (attachment.indexOf('https://blink-ct-ideas.storage.googleapis.com/') == 0) {
-    const blobId = attachment.substr('https://blink-ct-ideas.storage.googleapis.com/'.length);
-    console.log('resizing', blobId);
-    HTTP.post('https://blink-ct-3000.appspot.com/serveurl', {
-      params: {
-        bucket: 'blink-ct-ideas',
-        image: blobId
-      }
-    }, (err, res) => {
-      if (err) console.error(err);
-      if (res.content.indexOf('http://') == 0 && res.content.indexOf('googleusercontent.com/') >= 0) {
-        Ideas.update({_id:doc._id}, {$push:{images: res.content}});
-      } else {
-        console.error('Something error happens during image upload', res);
-      }
-    })
-  } else {
-    console.warn('Got an attachment not in googleapis', attachment);
-  }
+  attachments.forEach(attachment => {
+    if(attachment.indexOf('https://blink-ct-ideas.storage.googleapis.com/') === 0) {
+      const blobId = attachment.substr('https://blink-ct-ideas.storage.googleapis.com/'.length);
+      console.log('resizing', blobId);
+      HTTP.post('https://blink-ct-3000.appspot.com/serveurl', {
+        params: {
+          bucket: 'blink-ct-ideas',
+          image: blobId
+        }
+      }, (err, res) => {
+        if(err) console.error(err);
+        if(res.content.indexOf('http://') === 0 && res.content.indexOf('googleusercontent.com/') >= 0) {
+          Ideas.update({_id: doc._id}, {$push: {images: res.content}});
+        } else {
+          console.error('Something error happens during image upload', res);
+        }
+      });
+    } else {
+      console.warn('Got an attachment not in googleapis', attachment);
+    }
+  });
 });
