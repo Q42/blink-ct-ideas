7f99bd86f74300e7d31260bbbd354b7d8ed1057b
Joel de Nes <joeldenes@gmail.com>
2016-05-19 11:10:09 +0200

    Updates subscription fields

--- server/publications.js
+++ server/publications.js
@@ -2,7 +2,7 @@ import { Ideas } from '/imports/collections';
 
 const ITEMS_PER_PAGE = 10;
 const OVERVIEW_FIELDS = {authors: 1, title: 1, description: 1, images: 1, updatedDate: 1};
-const DETAIL_FIELDS = {authors: 1, title: 1, description: 1, images: 1, updatedDate: 1, reactions: 1};
+const DETAIL_FIELDS = {school: 1, authors: 1, title: 1, description: 1, images: 1, updatedDate: 1, reactions: 1};
 
 Meteor.publish('ideas.paged', (page: number = 1):object => {
   page = Math.max(page, 1);
