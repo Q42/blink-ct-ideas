2e926f94f46b56300dd90466da109896c81b8f63
Lukas van Driel <lukas@q42.nl>
2016-05-31 15:29:01 +0200

    geen settings deployen

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-DEPLOY_HOSTNAME=galaxy.meteor.com meteor deploy juniorcomputerlab.q42.nl --settings settings-prod.json --owner q42
+DEPLOY_HOSTNAME=galaxy.meteor.com meteor deploy juniorcomputerlab.q42.nl --owner q42
