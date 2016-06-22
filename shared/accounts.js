dd6d5abfffb3a6e607b29df3664d0316db507118
Lukas van Driel <lukas@q42.nl>
2016-06-22 11:25:40 +0200

    codeuur people can also log in as admin

--- shared/accounts.js
+++ shared/accounts.js
@@ -1,1 +1,3 @@
-Accounts.config({restrictCreationByEmailDomain:'q42.nl'});
+Accounts.config({restrictCreationByEmailDomain:function(address) {
+  return address.indexOf('@q42.nl') > 1 || address.indexOf('@codeuur.nl') > 1
+}});
